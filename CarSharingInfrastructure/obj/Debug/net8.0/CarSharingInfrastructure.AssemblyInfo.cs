073aeecb965f26101766980509147d64315d3ff5
Oleksandr Domanskyi <sasha2001do@gmail.com>
2024-02-16 17:35:20 +0100

    Some fixing in Edit View

--- CarSharingInfrastructure/obj/Debug/net8.0/CarSharingInfrastructure.AssemblyInfo.cs
+++ CarSharingInfrastructure/obj/Debug/net8.0/CarSharingInfrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarSharingInfrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3502229ad37b1f33fbd21f2a709cd443763dbf56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+570a93732336c12583973494bf28d18eb4dada2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarSharingInfrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarSharingInfrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
